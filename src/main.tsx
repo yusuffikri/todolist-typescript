4516d64cca4993e707b8d489d2d4a55d3c4aa0d2
yusuffikri <yusuffiks99@gmail.com>
2023-02-06 20:53:28 +0800

    heading text

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import theme from "./config/theme"
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import theme from './config/theme';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-    <App />
+      <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
